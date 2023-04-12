b5b300b1d7ecc7075a8ebd81b9a4be76dd3b8a98
Mushraf Khan <mushraf256@gmail.com>
2023-04-12 16:55:36 +0500

    initial commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function redirect()
+    {
+        $user_type=Auth::user()->user_type;
+        if ($user_type == '1') {
+            return view('admin.home');
+        } else {
+            return view('dashboard');
+            
+
+        }
+        
+    }
+    public function index()
+    {
+       return view('welcome');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
